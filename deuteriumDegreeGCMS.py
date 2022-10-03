b908125ed9c93a654f398a44b74df29533ba26ea
Nadine Hellmold <nadine.hellmold@gmail.com>
2022-10-03 22:34:04 +0200

    first working state

--- deuteriumDegreeGCMS.py
+++ deuteriumDegreeGCMS.py
@@ -4,26 +4,25 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-## process data (peak area) from gc-ms device
-
-# TODO wann sollte mittelwert gebildet werden, wann median oder ähnliches verwendet werden?
-
 # experiment needs to be the name of the folder containing all sample folders
 experiment = '20220907'
 testes_substrate = 'TBP'
-num_ions = 4
-peaks = 3
+
+if testes_substrate == 'TBP':
+    ion_types = {250: 'DD1', 252: 'DD2', 251: 'DD1', 253: 'DD2'}
+if testes_substrate == 'TCP':
+    'was anderes'
 
 directory = os.path.join(r'C:\Users\hellmold\Nextcloud\Experiments\Activity_Assay_GC_MS', experiment)
 
 # gives a list with all directories in my target/experiment folder
 samples = os.listdir(directory)
 
+
 if testes_substrate == 'TBP':
-    linenums = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-                32]  # altes file geht bis line 26, neus file hat mehr ionen bis line 32
+    linenums = [i for i in range(21, 33)]
 elif testes_substrate == 'TCP':
-    linenums = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+    linenums = [i for i in range(21, 31)]
 
 results = pd.DataFrame()
 # iterrate through list of samples and open result file
@@ -36,81 +35,99 @@ for sample in samples:
             for filename in files:
                 if filename == 'epatemp.txt':
                     data = open(os.path.join(directory, sample, 'epatemp.txt'))
+                    k=1
+                    a=0
                     for i, ln in enumerate(data):
                         if i in linenums:
                             new = ln.split(" ")
                             ion = (list(filter(lambda x: x != '', new))[1]).split('_')[1]
                             peakarea = (list(filter(lambda x: x != '', new))[4])
-                            results = results.append(
-                                {'Mastermix with': mastermix, 'timepoint [min]': time, 'replicate': replicate,
-                                 'ion [m/z]': ion, 'peakarea': peakarea}, ignore_index=True)
+                            RT = (list(filter(lambda x: x != '', new))[1]).split('_')[2]
+                            try:
+                                peakarea = float(peakarea)
+                            except Exception:
+                                peakarea = np.nan
+                            if k % 2 == 1:
+                                a = peakarea
+                            else:
+                                quotient = peakarea / (a + peakarea)
+                                if not np.isnan(quotient):
+                                    results = results.append(
+                                    {'Mastermix_with': mastermix, 'timepoint_[min]': time, 'replicate': replicate,
+                                     'ion': int(ion), 'quotient': quotient, 'RT': RT}, ignore_index=True)
+                            k = k + 1
 
-# adjust the timepoints to [min] as unit
+# add the ionpair assignment to dataframe
+results['ion_type'] = results.apply(lambda row: ion_types[row['ion']], axis=1)
+
+# adjust the timepoints to [min] as unit in dataframe
 for i in results.index:
-    time = results.loc[i, 'timepoint [min]']
+    time = results.loc[i, 'timepoint_[min]']
     if time == 'NCC' or time == 'NSC':
         continue
     newtime = re.findall('(\d+)', time)[0]
     unit = re.findall("[a-zA-Z]+", time)[0]
     if unit == 's' or unit == 'sec':
         newtime = int(newtime) / 60
-    results.loc[i, 'timepoint [min]'] = str(newtime)
+        newtime = ("%.2f" % newtime)                # keep only 2 digits after dot
+    results.loc[i, 'timepoint_[min]'] = float(newtime)
 
-#print(results)
 
-condition = pd.unique(results['Mastermix with'])
-stop_time = pd.unique(results['timepoint [min]'])
-replicates = pd.unique(results['replicate'])
-single_ions = pd.unique(results['ion [m/z]']) #evt wieder als int .asint oder so machen?
+calc = results.groupby(['Mastermix_with', 'timepoint_[min]', 'RT', 'ion_type']).quotient.agg('mean')
+calc = calc.reset_index()
+NCC = calc[(calc['timepoint_[min]'] == 'NCC')].index
+calc.drop(NCC, inplace=True)
+NSC = calc[(calc['timepoint_[min]'] == 'NSC')].index
+calc.drop(NSC, inplace=True)
+calc.sort_values(by=['timepoint_[min]'])
 
-# iter through every single timepoint result
-# calculate the DD
-for mastermix_condition in condition:
-    DeuteriumDegree = pd.DataFrame()
-    for time in stop_time:
-        # while repl increases, fill the lists/df
-        single_result = results.loc[
-            (results['Mastermix with'].eq(mastermix_condition)) & (results['timepoint [min]'].eq(time)), ['ion [m/z]', 'peakarea', 'replicate']]
-        single_result = single_result.replace('N.D.', np.nan)
-        single_result = single_result.apply(pd.to_numeric)
-        #print(type(single_result))
-        # wenn die anzahl der zeilen nur 1 replikat entspricht, dann, wenn drei replika in single result, dann was anderes
-        if len(single_result.index) == num_ions * peaks * 3:
-            single_result['peak'] = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]
-            print(single_result)
-        else:
-            single_result['peak'] = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]
-            print(single_result)
-        for peak in range(1, 4):
-            DD1_col = []
-            DD2_col = []
-            for repl in range(1, 4):
-                peakarea_array = []
-                for ion in single_ions:
-                    peakarea = single_result.loc[(single_result['ion [m/z]'] == int(ion)) & (single_result['peak'] == peak) & (single_result['replicate'] == repl)]
-                    try:
-                        peakarea = int(peakarea['peakarea'])
-                        peakarea_array.append(peakarea)
-                    except Exception:
-                        peakarea_array.append(np.nan)
-                DD1 = peakarea_array[1] / (peakarea_array[1] + peakarea_array[0])
-                DD2 = peakarea_array[3] / (peakarea_array[3] + peakarea_array[2])
-                DD1_col.append(DD1)
-                DD2_col.append(DD2)
-            DDs = [np.mean(DD1_col), np.mean(DD2_col)]
-            DeuteriumDegree[time] = [np.mean(DD1_col), np.mean(DD2_col)]
-    print(DeuteriumDegree)
-    # create plot
-    # get rid of non numeric entries NCC and NSC
-DeuteriumDegree = DeuteriumDegree.drop(['NCC', 'NSC'], axis=1)
-#print(DeuteriumDegree.dtypes)
-headerlist = list(DeuteriumDegree.columns.values)
-headerlist = sorted([float(x) for x in headerlist])     #die drecks headerlist wird in floats umgewandelt und sortiert
-DeuteriumDegree.columns = headerlist                    #die neude headerlist kommt auf den kack frame
-x = list(DeuteriumDegree.columns)
-y1 = DeuteriumDegree.iloc[0].tolist()
-y2 = DeuteriumDegree.iloc[1].tolist()
-plt.plot(x, y1, x, y2)
-plt.title('Mastermix w: '+ condition + ', ' + experiment + ', tested on: ' + testes_substrate)
-plt.savefig(os.path.join(r'C:\Users\hellmold\Nextcloud\Experiments\Activity_Assay_GC_MS', experiment, condition[0]))
-plt.show()
+# selects all data of calculated DD1 and DD2
+calc_DD1 = calc.loc[calc['ion_type'] == 'DD1']
+calc_DD2 = calc.loc[calc['ion_type'] == 'DD2']
+condition = pd.unique(results['Mastermix_with'])
+
+# for every MM approach
+# figure 1, 2, 3: DD1, DD2 at RT1, RT2, RT3
+# figure 4: DD1 and DD2 for average of all RT's
+for MM in condition:
+    for x in range(1, 4):
+        calc_DD1_RT = calc_DD1.loc[(calc_DD1['RT'] == str(x)) & (calc_DD1['Mastermix_with'] == MM)]
+        calc_DD2_RT = calc_DD2.loc[(calc_DD2['RT'] == str(x)) & (calc_DD2['Mastermix_with'] == MM)]
+
+        x1 = list(calc_DD1_RT.iloc[:, 1])
+        y1 = list(calc_DD1_RT.iloc[:, 4])
+        x2 = list(calc_DD2_RT.iloc[:, 1])
+        y2 = list(calc_DD2_RT.iloc[:, 4])
+
+        plt.plot(x1, y1, label='DD1')
+        plt.plot(x2, y2, label='DD2')
+        plt.title('DD1, DD2 at RT ' + str(x) + ' Mastermix w: ' + MM + ', ' + experiment + ', ' + testes_substrate)
+        plt.xlabel('time [min]')
+        plt.ylabel('Deuterium Degree')
+        plt.legend(loc="upper right")
+        plt.savefig(os.path.join(r'C:\Users\hellmold\Nextcloud\Experiments\Activity_Assay_GC_MS', experiment, str(x)) + MM)
+        plt.show()
+
+    #figure 4: AVG(DD1, DD2)
+    calc_MW = calc.groupby(['Mastermix_with', 'timepoint_[min]', 'ion_type']).quotient.agg('mean')
+    calc_MW = calc_MW.reset_index()
+
+    calc_MW_DD1 = calc_MW.loc[(calc_MW['ion_type'] == 'DD1') & (calc_MW['Mastermix_with'] == MM)]
+    calc_MW_DD2 = calc_MW.loc[(calc_MW['ion_type'] == 'DD2') & (calc_MW['Mastermix_with'] == MM)]
+
+    x1 = list(calc_MW_DD1.iloc[:, 1])
+    y1 = list(calc_MW_DD1.iloc[:, 3])
+    x2 = list(calc_MW_DD2.iloc[:, 1])
+    y2 = list(calc_MW_DD2.iloc[:, 3])
+
+    plt.plot(x1, y1, label='DD1')
+    plt.plot(x2, y2, label='DD2')
+    plt.title('DD1, DD2 all RTs ' + ' Mastermix w: ' + MM + ', ' + experiment + ', ' + testes_substrate)
+    plt.xlabel('time [min]')
+    plt.ylabel('Deuterium Degree')
+    plt.legend(loc="upper right")
+    plt.savefig(os.path.join(r'C:\Users\hellmold\Nextcloud\Experiments\Activity_Assay_GC_MS', experiment, 'MW' + MM))
+    plt.show()
+
+
+
